2069bee5540ce4886cec222771e60fa6cc731f22
MohamedAthif2005 <comradeathif@gmail.com>
2025-11-23 19:13:33 +0530

    Second commit minor changes

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-function sendMail() {
+function sendMail(){
     let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    };
-
-    emailjs.send("service_7ucxofz", "template_a639kg9", params)
-        .then((res) => {
-            alert("Email sent successfully!");
-            console.log(res);
-        })
-        .catch((err) => {
-            alert("Something went wrong");
-            console.log(err);
-        });
+        name : document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value
+    }
+    emailjs.send("service_7ucxofz","template_a639kg9",params)
+    .then(() => alert("Email sent successfully"))
+    .catch(() => alert("Something went wrong"));
 }
+
+const ham = document.querySelector(".hamburger");
+const overlay = document.querySelector(".menu-overlay");
+const overlayLinks = document.querySelectorAll(".menu-overlay a");
+
+ham.addEventListener("click", () => {
+    ham.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlayLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        ham.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
