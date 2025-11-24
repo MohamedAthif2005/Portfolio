886520f649c5541d763beee1af4b91f8ce6bdac1
MohamedAthif2005 <comradeathif@gmail.com>
2025-11-24 19:21:52 +0530

    Contact form updated

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function sendMail(){
-    let params = {
-        name : document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value
+function sendMail() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const type = document.querySelector(".type");
+
+    if (!name || !email || !message) {
+        type.textContent = "Please fill all the fields required";
+        return;
     }
+    if (!email.includes("@")) {
+        type.textContent = "Invalid email";
+        return;
+    }
+
+    let params = { name, email, message };
+
     emailjs.send("service_7ucxofz","template_a639kg9",params)
     .then(() => sent())
     .catch(() => alert("Something went wrong"));
@@ -12,18 +23,10 @@ const form = document.querySelector(".contact-form");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    sendMail();         
+    sendMail();  
+    form.reset();       
 });
-function sent() {
-    const type = document.querySelector(".type");
 
-    type.textContent = "Email Sent Successfully 😊";
-
-    setTimeout(() => {
-        type.textContent = "";
-    }, 3000);
-    form.reset();
-}
 function shownav()
 {
     const ham = document.querySelector(".mobile-nav");
